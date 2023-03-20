de4eb76e9ea6a3bb30c911ecd781891e71d1439a
Acid Chicken (硫酸鶏) <root@acid-chicken.com>
2023-03-21 00:06:07 +0900

    feat: init

--- src/Tool.tsx
+++ src/Tool.tsx
@@ -1,38 +1,67 @@
-import React, { memo, useCallback, useEffect } from "react";
+import React, { memo, useState } from "react";
 import { useGlobals, useStorybookApi } from "@storybook/manager-api";
-import { Icons, IconButton } from "@storybook/components";
+import { Icons, IconButton, TooltipLinkList, WithTooltip } from "@storybook/components";
 import { ADDON_ID, PARAM_KEY, TOOL_ID } from "./constants";
 
 export const Tool = memo(function MyAddonSelector() {
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
   const [globals, updateGlobals] = useGlobals();
   const api = useStorybookApi();
 
-  const isActive = [true, "true"].includes(globals[PARAM_KEY]);
-
-  const toggleMyTool = useCallback(() => {
-    updateGlobals({
-      [PARAM_KEY]: !isActive,
-    });
-  }, [isActive]);
-
-  useEffect(() => {
-    api.setAddonShortcut(ADDON_ID, {
-      label: "Toggle Measure [O]",
-      defaultShortcut: ["O"],
-      actionName: "outline",
-      showInMenu: false,
-      action: toggleMyTool,
-    });
-  }, [toggleMyTool, api]);
+  const isActive = !!globals[PARAM_KEY];
 
   return (
-    <IconButton
-      key={TOOL_ID}
-      active={isActive}
-      title="Enable my addon"
-      onClick={toggleMyTool}
+    <WithTooltip
+      placement="top"
+      closeOnOutsideClick
+      tooltip={({ onHide }) => {
+        return (
+          <TooltipLinkList
+            links={[
+              '(clear)',
+              'l-light',
+              'l-coffee',
+              'l-apricot',
+              'l-rainy',
+              'l-botanical',
+              'l-vivid',
+              'l-cherry',
+              'l-sushi',
+              'l-u0',
+              'd-dark',
+              'd-persimmon',
+              'd-astro',
+              'd-future',
+              'd-botanical',
+              'd-green-lime',
+              'd-green-orange',
+              'd-cherry',
+              'd-ice',
+              'd-u0',
+            ].map((theme, i) => ({
+              id: theme,
+              title: theme,
+              onClick() {
+                updateGlobals({
+                  [PARAM_KEY]: i ? theme : false,
+                });
+                onHide();
+              },
+              value: theme,
+              active: theme === globals[PARAM_KEY],
+            }))}
+          />
+        );
+      }}
+      onVisibleChange={setIsTooltipVisible}
     >
-      <Icons icon="lightning" />
-    </IconButton>
+      <IconButton
+        key={TOOL_ID}
+        active={isActive || isTooltipVisible}
+        title="Switch theme"
+      >
+        <Icons icon="mirror" />
+      </IconButton>
+    </WithTooltip>
   );
 });
